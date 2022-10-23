08fbf06c16f8f0a80f93f69e238f0a03b9361854
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2022-10-23 09:20:31 +0700

    edit final submission

--- belajar-event-js/script.js
+++ belajar-event-js/script.js
@@ -2,26 +2,21 @@ const grandparent = document.querySelector('.grandparent')
 const parent = document.querySelector('.parent')
 const child = document.querySelector('.child')
 
-grandparent.addEventListener('click', (e) => {
-  console.log('grandparent');
-}, { capture : true });
 
-parent.addEventListener('click', () => {
-  console.log('parent');
-}, { capture : true })
-
-child.addEventListener('click', () => {
-  console.log('child');
-}, { capture : true })
-
-grandparent.addEventListener('click', (e) => {
+grandparent.addEventListener('click', () => {
+  // target = grandparent;
   console.log('grandparent');
 });
 
-parent.addEventListener('click', () => {
-  console.log('parent');
-})
+// parent.addEventListener('click', (e) => {
+//   e.stopPropagation()
+//   console.log('parent');
+// },);
 
-child.addEventListener('click', () => {
-  console.log('child');
-})
+// child.addEventListener('click', (e) => {
+//   e.stopPropagation()
+//   console.log('child');
+// },);
+
+// console.log(e.target);
+// console.log(target);
