08e561686dbc05bee48261ddddeb0dcf1ee522c1
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-21 11:03:41 +0700

    memantapkan konsep slice js

--- array js/slice.js
+++ array js/slice.js
@@ -1,3 +1,17 @@
-let name = ['Adi', 'Muhamad', 'Firmaansyah', 'Hidayah'];
-name = name.slice(0,2);
-console.log(name);
+const name = ['Adi', 'Muhamad', 'Firmaansyah', 'Hidayah'];
+console.log(name.slice(0,2));
+// output : [ 'Adi', 'Muhamad' ]
+// dibaca na teh dipotong dari mulai index 0 sampai sebelum index 2, ambil potongan itu 
+console.log(name.slice(2));
+// output : [ 'Muhamad', 'Firmaansyah', 'Hidayah' ]
+// ieu mah dibacana berarti buang dari mulai index sebelum 1
+
+// oke ieu berarti lain itungan index tapi siga itungan garis tiap value tina index na.
+// | Adi, | Muhamad, | Firmansyah |
+// 0      1          2            3
+// lamun slice(0,2) maka nu diambilna
+// 0-----1-----2 nilai antara border itu bakal dimbil
+
+// lamun slice(2) berarti
+// mulai ti 0 sampai 2
+// 0---1---2 nialinya akan dibuang
