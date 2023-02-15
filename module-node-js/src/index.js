949607fa5ec80060ad24d1fdc29e17a15c4558bb
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-02-15 14:39:29 +0700

    nambahin bebearapa

---
+++ module-node-js/src/index.js
@@ -0,0 +1,6 @@
+import name from './name.js';
+
+console.log(name);
+
+// import harus pakai .js sia teh koplok
+// ternyata fungsi dari export default itu supaya pas ngimport bisa ngenamain bebas karena toh cumah export 1
---
+++ module-node-js/src/name.js
@@ -0,0 +1,3 @@
+const name = 'Adi';
+
+export default  name;
