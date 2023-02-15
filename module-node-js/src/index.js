08d55e3aa3e49e4f0b6e1a9ad5e09fa63c6ec2fe
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-02-15 15:03:26 +0700

    nambahin catatan tentang import export

--- module-node-js/src/index.js
+++ module-node-js/src/index.js
@@ -4,3 +4,12 @@ console.log(name);
 
 // import harus pakai .js sia teh koplok
 // ternyata fungsi dari export default itu supaya pas ngimport bisa ngenamain bebas karena toh cumah export 1
+
+// import itu pasangannya export atau export default?
+/**
+ * apa bedanya export sama export defaul?
+ * export itu nge export dengan mewajibkan import untuk menggunakan nama dari variable yang di export
+ * sedangkan export default saat mengimport bisa menggunakan nama lain untuk menamai ulang variable export
+ * 
+ * EXPORT DEFAULT GAK BISA PAKAI IMPORT * AS inget!
+ */
